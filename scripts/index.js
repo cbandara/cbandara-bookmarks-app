9e67d13ba4c6444ecbeaa2cb5f7d76dcf4500f22
Charutha Bandara <charuthabandara@gmail.com>
2018-08-05 13:28:34 -0400

    removed console logs

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,7 @@
 /* global $, bookmarkList, store, api */
 
 $(document).ready(function() {
-  console.log('document is ready');
   bookmarkList.bindEventListeners();
-  console.log('finished running bindEventListeners');
-  console.log('Moving onto api.getBookmarks');
   api.getBookmarks((bookmarks) => {
     bookmarks.forEach((bookmark) => bookmark.expanded = false);
     bookmarks.forEach((bookmark) => store.addBookmark(bookmark));
